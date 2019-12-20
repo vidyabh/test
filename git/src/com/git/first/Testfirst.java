728a837adb263ad212ca904d77c44c05e683ed44
HP <HP@DESKTOP-PSIDAIQ>
2019-12-20 14:51:01 +0530

    git main

--- git/src/com/git/first/Testfirst.java
+++ git/src/com/git/first/Testfirst.java
@@ -4,6 +4,7 @@ public class Testfirst {
 
 	public static void main(String[] args) {
 		System.out.println("hi, wellcome");
+		System.out.println("this is main");
 		// TODO Auto-generated method stub
 
 	}
